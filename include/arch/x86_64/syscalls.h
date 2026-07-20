f0baf057e5605351cdd7dc40198cdd34f890377e
voidPointer <mobyclub11062@gmail.com>
2026-07-20 18:25:48 +0300

    add syscall registers struct

--- include/arch/x86_64/syscalls.h
+++ include/arch/x86_64/syscalls.h
@@ -16,3 +16,8 @@ typedef struct {
     uint64_t user_rsp;
     uint64_t kernel_rsp;
 }__attribute__((packed)) syscalls_stacks_t;
+
+struct SyscallRegisters {
+    uint64_t rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15;
+    uint64_t rip, rflags;
+}__attribute__((packed));
