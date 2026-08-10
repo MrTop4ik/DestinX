38b7d30dcf917681943cb10b312a6aed82e095d6
MrTop4ik <mobyclub11062@gmail.com>
2026-08-10 17:14:53 +0300

    add pci write and read dword

--- include/arch/x86_64/drivers/pci.h
+++ include/arch/x86_64/drivers/pci.h
@@ -4,3 +4,8 @@
 #include <mm/vmalloc.h>
 #include <arch/x86_64/drivers/serial.h>
 
+#define PCI_CONFIG_ADDRESS 0xCF8
+#define PCI_CONFIG_DATA    0xCFC
+
+void pci_write_dword(uint8_t bus, uint8_t dev, uint8_t func, uint8_t offset, uint32_t value);
+uint32_t pci_read_dword(uint8_t bus, uint8_t dev, uint8_t func, uint8_t offset);
