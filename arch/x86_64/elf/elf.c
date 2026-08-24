1f0f5a67baef4b8ea44bf3f6cf8479d9a105274c
MrTop4ik <mobyclub11062@gmail.com>
2026-08-24 09:47:02 +0300

    fix elf loader

--- arch/x86_64/elf/elf.c
+++ arch/x86_64/elf/elf.c
@@ -15,7 +15,7 @@ Elf64_Addr load_elf(uint8_t *elf_file_data){
         if (phdr[i].p_type == PT_LOAD){
             uint64_t pages_needed = (phdr[i].p_memsz + PAGE_SIZE_4KB - 1) / PAGE_SIZE_4KB;
             uint64_t paddr = pmm_alloc_pages(pages_needed);
-            for (int j = 0; j < pages_needed; i ++) vmm_map_page(read_cr3(), paddr + (j * PAGE_SIZE_4KB), phdr[i].p_vaddr + (j * PAGE_SIZE_4KB), PAGE_SIZE_4KB, (PTE_WRITABLE | PTE_USER));
+            for (int j = 0; j < pages_needed; j++) vmm_map_page(read_cr3(), paddr + (j * PAGE_SIZE_4KB), phdr[i].p_vaddr + (j * PAGE_SIZE_4KB), PAGE_SIZE_4KB, (PTE_WRITABLE | PTE_USER));
 
             if (phdr[i].p_filesz > 0) memcpy((void*)phdr[i].p_vaddr, (void*)(elf_file_data + phdr[i].p_offset), phdr[i].p_filesz);
             if (phdr[i].p_memsz > phdr[i].p_filesz) memset((void*)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
