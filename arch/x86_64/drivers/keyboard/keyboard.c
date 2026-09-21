8096fc689f4111183d64e18e90114955dea76f0e
MrTop4ik <mobyclub11062@gmail.com>
2026-09-21 18:33:34 +0300

    add support for extended scancodes

--- arch/x86_64/drivers/keyboard/keyboard.c
+++ arch/x86_64/drivers/keyboard/keyboard.c
@@ -2,63 +2,48 @@
 
 int shift;
 int capslock;
+int ext;
 
-const uint32_t UNKNOWN = 0xFFFFFFFF;
-const uint32_t ESC = 0xFFFFFFFF - 1;
-const uint32_t CTRL = 0xFFFFFFFF - 2;
-const uint32_t LSHFT = 0xFFFFFFFF - 3;
-const uint32_t RSHFT = 0xFFFFFFFF - 4;
-const uint32_t ALT = 0xFFFFFFFF - 5;
-const uint32_t F1 = 0xFFFFFFFF - 6;
-const uint32_t F2 = 0xFFFFFFFF - 7;
-const uint32_t F3 = 0xFFFFFFFF - 8;
-const uint32_t F4 = 0xFFFFFFFF - 9;
-const uint32_t F5 = 0xFFFFFFFF - 10;
-const uint32_t F6 = 0xFFFFFFFF - 11;
-const uint32_t F7 = 0xFFFFFFFF - 12;
-const uint32_t F8 = 0xFFFFFFFF - 13;
-const uint32_t F9 = 0xFFFFFFFF - 14;
-const uint32_t F10 = 0xFFFFFFFF - 15;
-const uint32_t F11 = 0xFFFFFFFF - 16;
-const uint32_t F12 = 0xFFFFFFFF - 17;
-const uint32_t SCRLCK = 0xFFFFFFFF - 18;
-const uint32_t HOME = 0xFFFFFFFF - 19;
-const uint32_t UP = 0xFFFFFFFF - 20;
-const uint32_t LEFT = 0xFFFFFFFF - 21;
-const uint32_t RIGHT = 0xFFFFFFFF - 22;
-const uint32_t DOWN = 0xFFFFFFFF - 23;
-const uint32_t PGUP = 0xFFFFFFFF - 24;
-const uint32_t PGDOWN = 0xFFFFFFFF - 25;
-const uint32_t END = 0xFFFFFFFF - 26;
-const uint32_t INS = 0xFFFFFFFF - 27;
-const uint32_t DEL = 0xFFFFFFFF - 28;
-const uint32_t CAPS = 0xFFFFFFFF - 29;
-const uint32_t NONE = 0xFFFFFFFF - 30;
-const uint32_t ALTGR = 0xFFFFFFFF - 31;
-const uint32_t NUMLCK = 0xFFFFFFFF - 32;
-
-
-const uint32_t lowercase[128] = {
-    UNKNOWN,ESC,'1','2','3','4','5','6','7','8',
-    '9','0','-','=','\b','\t','q','w','e','r',
-    't','y','u','i','o','p','[',']','\n',CTRL,
-    'a','s','d','f','g','h','j','k','l',';',
-    '\'','`',LSHFT,'\\','z','x','c','v','b','n','m',',',
-    '.','/',RSHFT,'*',ALT,' ',CAPS,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,NUMLCK,SCRLCK,HOME,UP,PGUP,'-',LEFT,UNKNOWN,RIGHT,
-    '+',END,DOWN,PGDOWN,INS,DEL,UNKNOWN,UNKNOWN,UNKNOWN,F11,F12,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN
+const char *lowercase[] = {
+    "UNKNOWN","ESC","1","2","3","4","5","6","7","8",
+    "9","0","-","=","\b","\t","q","w","e","r",
+    "t","y","u","i","o","p","[","]","\n","CTRL",
+    "a","s","d","f","g","h","j","k","l",";",
+    "\"","`","LSHFT","\\","z","x","c","v","b","n","m",",",
+    ".","/","RSHFT","*","ALT"," ","CAPS","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","NUMLCK","SCRLCK","HOME","UP","PGUP","-","LEFT","UNKNOWN","RIGHT",
+    "+","END","DOWN","PGDOWN","INS","DEL","UNKNOWN","UNKNOWN","UNKNOWN","F11","F12","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN"
 };
 
-const uint32_t uppercase[128] = {
-    UNKNOWN,ESC,'!','@','#','$','%','^','&','*','(',')','_','+','\b','\t','Q','W','E','R',
-    'T','Y','U','I','O','P','{','}','\n',CTRL,'A','S','D','F','G','H','J','K','L',':','"','~',LSHFT,'|','Z','X','C',
-    'V','B','N','M','<','>','?',RSHFT,'*',ALT,' ',CAPS,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,NUMLCK,SCRLCK,HOME,UP,PGUP,'-',
-    LEFT,UNKNOWN,RIGHT,'+',END,DOWN,PGDOWN,INS,DEL,UNKNOWN,UNKNOWN,UNKNOWN,F11,F12,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,
-    UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN
+const char *uppercase[] = {
+    "UNKNOWN","ESC","!","@","#","$","%","^","&","*","(",")","_","+","\b","\t","Q","W","E","R",
+    "T","Y","U","I","O","P","{","}","\n","CTRL","A","S","D","F","G","H","J","K","L",":","\"","~","LSHFT","|","Z","X","C",
+    "V","B","N","M","<",">","?","RSHFT","*","ALT"," ","CAPS","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","NUMLCK","SCRLCK","HOME","UP","PGUP","-",
+    "LEFT","UNKNOWN","RIGHT","+","END","DOWN","PGDOWN","INS","DEL","UNKNOWN","UNKNOWN","UNKNOWN","F11","F12","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN",
+    "UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN","UNKNOWN"
+};
+
+const char* extended[] = {
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "Keypad Enter", "RCtrl", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "Keypad /", "", "", "RAlt", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "Home", "Up Arrow", "Page Up", "", "Left Arrow", "", "Right Arrow", "", "End",
+    "Down Arrow", "Page Down", "Insert", "Delete", "", "", "", "", "", "", "", "Left Super", "Right Super", "Apps/Menu", "Power", "Sleep",
+    "", "", "", "Wake", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
 };
 
 void keyboard_handler(struct InterruptRegisters *regs){
@@ -66,7 +51,7 @@ void keyboard_handler(struct InterruptRegisters *regs){
     uint8_t scanCode = rawCode & 0x7F;
     int press = rawCode & 0x80;
 
-    if (rawCode == 0xE0) return;
+    if (rawCode == 0xE0) { ext = 1; return; }
 
     switch(scanCode){
         case 1:
@@ -97,7 +82,10 @@ void keyboard_handler(struct InterruptRegisters *regs){
             break;
         
         default:
-            if (!press) serial_print("%s", ((!shift && !capslock) || (shift && capslock)) ? &lowercase[scanCode] : &uppercase[scanCode]);
+            if (!press){
+                if (ext) { ext = 0; serial_print("%s\n", extended[scanCode]); }
+                else serial_print("%s\n", ((!shift && !capslock) || (shift && capslock)) ? lowercase[scanCode] : uppercase[scanCode]);
+            }
             break;
     }
 
@@ -107,6 +95,7 @@ void keyboard_handler(struct InterruptRegisters *regs){
 void init_keyboard(void){
     shift = 0;
     capslock = 0;
+    ext = 0;
     setIRQHandler(1, &keyboard_handler);
     ioapic_set_irq(1, 0x21, 0);
 }
